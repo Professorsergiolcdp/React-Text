fe1c80501a4633f7210d8d8ddd0fc718270e444b
Shiva Patel <yehamarailakahai@gmail.com>
2021-09-01 13:36:55 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+// import About from './component/About';
+import Navbar from './component/Navbar';
+import TextForm from './component/TextForm';
+import React, { useState } from 'react'
 
 function App() {
+
+ const [mode,setMode] = useState('light');
+
+  const toggleMode = () =>{
+    if(mode === 'light'){
+      setMode('dark');
+      var target = document.getElementById('my-box');
+      target.style.backgroundColor = 'grey';
+      target.style.color = 'grey';
+    }else{
+      setMode('light');
+      var target = document.getElementById('my-box');
+      target.style.backgroundColor = 'white';
+      target.style.color = 'black';
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <>
+        <Navbar  aboutText = "About us" mode={mode} toggleMode={toggleMode}/>
+        <div className="container my-3">
+             <TextForm placeholder = "Enter the Text"/>
+             {/* <About/> */}
+        </div>
+      </>
   );
 }
 
